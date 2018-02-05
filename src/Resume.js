b9fff8573c980bd56bb438f061b1ee8d7d062af9
Innovationbham_3 <Innovationbham_3@UABiLab-3s-iMac.local>
2018-02-05 10:34:03 -0600

    initial commit

---
+++ src/Resume.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Resume.css';
+
+class Resume extends Component {
+  render() {
+    return (
+      <div className="Resume">
+        <header className="Resume-header">
+          <h1 className="Resume-title">Welcome to React</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Resume;
